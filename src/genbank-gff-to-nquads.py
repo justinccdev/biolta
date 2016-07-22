57350a2e016a09839fca615a1fb7d36246cf9b2d
Justin Clark-Casey <justincc@justincc.org>
2016-07-22 19:09:02 +0100

    Make genbank-gff-to-nquads.py spit out an n-quads file associating genbank accession number with gene locus tags

--- src/genbank-gff-to-nquads.py
+++ src/genbank-gff-to-nquads.py
@@ -4,6 +4,7 @@ import jargparse
 
 parser = jargparse.ArgParser('Convert Genbank GFF into an n-quad file')
 parser.add_argument('gffPath', help='path to the GFF')
+parser.add_argument('outPath', help='path to output the n-quads')
 args = parser.parse_args()
 
 metadataPrefix = '#'
@@ -11,6 +12,7 @@ accessionKey = '#!genome-build-accession NCBI_Assembly:'
 locusTagAttributeKey = 'locus_tag='
 
 accessionIdentifier = 'NONE FOUND'
+locusTags = []
 
 with open(args.gffPath) as f:
     for line in f:
@@ -28,7 +30,8 @@ with open(args.gffPath) as f:
                 for a in attributes:
                     # print a
                     if a.startswith(locusTagAttributeKey):
-                        locusTag = a[len(locusTagAttributeKey):]
-                        print locusTag
+                        locusTags.append(a[len(locusTagAttributeKey):])
 
-print 'accessionIdentifier [%s]' % accessionIdentifier
+with open(args.outPath, 'w') as f:
+    for locusTag in locusTags:
+        f.write('<%s> <haslocus> "%s" .\n' % (accessionIdentifier, locusTag))
