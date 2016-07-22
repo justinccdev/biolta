fc7a8451ba086675b264a73d132828c245acd534
Justin Clark-Casey <justincc@justincc.org>
2016-07-22 18:48:24 +0100

    Pluck out raw attributes

--- src/genbank-gff-to-nquads.py
+++ src/genbank-gff-to-nquads.py
@@ -6,12 +6,18 @@ parser = jargparse.ArgParser('Convert Genbank GFF into an n-quad file')
 parser.add_argument('gffPath', help='path to the GFF')
 args = parser.parse_args()
 
+metadataPrefix = '#'
 accessionKey = '#!genome-build-accession NCBI_Assembly:'
 accessionIdentifier = 'NONE FOUND'
 
 with open(args.gffPath) as f:
     for line in f:
-        if line.startswith(accessionKey):
-            accessionIdentifier = line[len(accessionKey):]
+        line = line.strip()
+        if line.startswith(metadataPrefix):
+            if line.startswith(accessionKey):
+                accessionIdentifier = line[len(accessionKey):]
+        else:
+            components = line.split()
+            print components[8]
 
-print accessionIdentifier
+print 'accessionIdentifier [%s]' % accessionIdentifier
