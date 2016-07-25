d8b63c93d424afd3d66483b8b9f153c6153eb0c3
Justin Clark-Casey <justincc@goose.sysbiol.private.cam.ac.uk>
2016-07-25 13:32:35 +0100

    factor out record parsing method

--- src/genbank-gff-to-nquads.py
+++ src/genbank-gff-to-nquads.py
@@ -2,14 +2,33 @@
 
 import jargparse
 
+#################
+### CONSTANTS ###
+#################
+metadataPrefix = '#'
+accessionKey = '#!genome-build-accession NCBI_Assembly:'
+locusTagAttributeKey = 'locus_tag'
+
+#################
+### FUNCTIONS ###
+#################
+def parseRecord(record, locusTags):
+    components = record.split()
+    type = components[2]
+    rawAttributes = components[8]
+
+    if type == 'gene':
+        attributes = rawAttributes.split(';')
+        for a in attributes:
+            (key, value) = a.split('=')
+            # print a
+            if key == locusTagAttributeKey:
+                locusTags.append(value)
+
 parser = jargparse.ArgParser('Convert Genbank GFF into an n-quad file')
 parser.add_argument('gffPath', help='path to the GFF')
 parser.add_argument('outPath', help='path to output the n-quads')
 args = parser.parse_args()
-
-metadataPrefix = '#'
-accessionKey = '#!genome-build-accession NCBI_Assembly:'
-locusTagAttributeKey = 'locus_tag'
 
 accessionIdentifier = 'NONE FOUND'
 locusTags = []
@@ -21,17 +40,7 @@ with open(args.gffPath) as f:
             if line.startswith(accessionKey):
                 accessionIdentifier = line[len(accessionKey):]
         else:
-            lineComponents = line.split()
-            type = lineComponents[2]
-            rawAttributes = lineComponents[8]
-
-            if type == 'gene':
-                attributes = rawAttributes.split(';')
-                for a in attributes:
-                    (key, value) = a.split('=')
-                    # print a
-                    if key == locusTagAttributeKey:
-                        locusTags.append(value)
+            parseRecord(line, locusTags)
 
 with open(args.outPath, 'w') as f:
     for locusTag in locusTags:
